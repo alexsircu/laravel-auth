ffc00a50dea6614ec63e75310efa96d6df9aaee6
Alex Sircu <alex.sricu@gmail.com>
2021-02-25 16:28:00 +0100

    Creato model Post e controller dentro cartella Admin

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,17 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// tutto quello che dovrà caricare farà parte della cartella admin
+Route::prefix('admin')  //prefisso della rotta
+    ->namespace('Admin')  //definisce la cartella in cui ho salvato il mio controller
+    ->middleware('auth')    //filtro per autentificazione
+    ->name('admin.')    //il nome della rotta sarà ad esempio admin.posts.index perchè avrò anche delle rotte pubbliche con la stessa rotta posts.index
+    ->group(function() {
+
+        Route::resource('posts', 'PostController'); //ora questa istruzione creerà tutte le rotte con il prefisso admin
+
+        //posso inserire anche altre rotte perchè sono dentro ad un gruppo di rotte
+
+});
+
